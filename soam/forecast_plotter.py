1793c5554d03d8863d51dabe81c29f6116da78a1
grog <francisco@muttdata.ai>
2020-09-14 21:13:27 -0300

    Include forecast_plotter.py numpydoc.

--- soam/forecast_plotter.py
+++ soam/forecast_plotter.py
@@ -1,61 +1,61 @@
-# forecast_plotter.py
-
+"""
+Forecast Plotter
+----------
+Postprocess to plot the model forecasts.
+"""
 import logging
 from pathlib import Path
-from typing import Union
+from typing import Union, NoReturn
 
 import pandas as pd
-from soam.constants import DAILY_TIME_GRANULARITY, DS_COL, FORECAST_DATE, PARENT_LOGGER
+
+from soam.constants import (DAILY_TIME_GRANULARITY, DS_COL, FORECAST_DATE,
+                            PARENT_LOGGER)
 from soam.plot_utils import create_forecast_figure
 from soam.utils import get_file_path
 
 logger = logging.getLogger(f"{PARENT_LOGGER}.{__name__}")
 
 
-"""
-ForecastPlotter
-----------
-In charge of formatting and plotting the results of a fitted model.
-"""
-
-
 class ForecastPlotter:
-    def __init__(self, path: Union[Path, str], metric_name: str):
-        """
-        Formats and plots the forecaster passed as a parameter.
-        
+    """
+    ForecastPlotter
+    ----------
+    In charge of formatting and plotting the results of a fitted model.
+    """
+
+    def __init__(self, path: Union[Path, str], metric_name: str) -> NoReturn:
+        """Formats and plots the forecaster passed as a parameter.
+
         Parameters
         ----------
-        path
-            str or pathlib.Path where the plots will be stored.
+        path : str or pathlib.Path
+            Where the plots will be stored.
+        metric_name : str
+            The name for the metric that its going to plot.
         """
         self.path = Path(path)
         self.metric_name = metric_name
 
-    def plot(
-        self,
-        raw_series: pd.DataFrame,
-        predictions: pd.DataFrame,
-        time_granularity: str = DAILY_TIME_GRANULARITY,
-    ) -> Path:
-        """
-        Create and store the result plot in the constructed path.
+    def plot(self, raw_series: pd.DataFrame, predictions: pd.DataFrame,
+             time_granularity: str = DAILY_TIME_GRANULARITY) -> Path:
+        """Create and store the result plot in the constructed path.
         
         If the path does not exist, it will be created.
 
         Parameters
         ----------
-        raw_series
-            Dataframe belonging to a raw_series of data.
-        predictions
-            Dataframe with the result of the predictions.
-        time_granularity
+        raw_series : pandas.Dataframe
+            The raw_series of data.
+        predictions : pandas.Dataframe
+            The result of the predictions.
+        time_granularity : str
             Time granularity of the series (daily or hourly)
+
         Returns
         -------
-        Path
+        pathlib.Path
             The path of the resulting plot
-
         """
         predictions[DS_COL] = predictions[FORECAST_DATE]
 
@@ -74,9 +74,8 @@ class ForecastPlotter:
         )
 
         self.path.mkdir(parents=True, exist_ok=True)
-        fn = "_".join(
-            ["forecast", f"{start_date:%Y%m%d%H}", f"{end_date:%Y%m%d%H}", ".png"]
-        )
+        fn = "_".join(["forecast", f"{start_date:%Y%m%d%H}",
+                       f"{end_date:%Y%m%d%H}", ".png"])
         plot_path = get_file_path(self.path, fn)
         logger.debug(f"Saving forecast figure to {plot_path}...")
         fig.savefig(plot_path, bbox_inches="tight")
