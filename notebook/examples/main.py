b28baed1aa9c4c6b461343042b84398ecf5096cb
Pablo Lorenzatto <pablo.a.lorenzatto@gmail.com>
2021-02-28 20:02:41 -0300

    fix lint

--- notebook/examples/main.py
+++ notebook/examples/main.py
@@ -7,11 +7,11 @@ import pandas as pd
 from prefect import task
 
 from soam.cfg import get_db_cred
+from soam.core import SoamFlow
 from soam.forecaster import Forecaster
 from soam.plotting import ForecastPlotterTask
-from soam.runner import SoamFlow
+from soam.reporting import SlackReportTask
 from soam.savers import CSVSaver, DBSaver
-from soam.slack_report import SlackReportTask
 
 logger = logging.getLogger(__name__)
 
