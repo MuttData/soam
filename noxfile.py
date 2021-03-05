2987538927c1dc1b1eb130b4fa6e29bc9397cd3f
Diego Lizondo <lizondo.diego@gmail.com>
2021-03-05 16:20:58 -0300

    Added --no-cov flat to notebook's test cmd.

--- noxfile.py
+++ noxfile.py
@@ -16,7 +16,13 @@ def tests(session):
     session.run(*cmd)
 
     session.run(
-        "python", "-m", "pytest", "--nbval-lax", "notebook/examples/", "--current-env"
+        "python",
+        "-m",
+        "pytest",
+        "--nbval-lax",
+        "notebook/examples/",
+        "--current-env",
+        "--no-cov",
     )
 
 
