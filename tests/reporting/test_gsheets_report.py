968b0ccaf7c5afd434c1fdfb09527d2967105798
Guido Trucco <guido@muttdata.ai>
2021-03-08 14:52:58 -0300

    test_init_fails_if_config_path_is_not_json

--- tests/reporting/test_gsheets_report.py
+++ tests/reporting/test_gsheets_report.py
@@ -1,3 +1,5 @@
+from json.decoder import JSONDecodeError
+
 import pytest
 
 from soam.reporting import GSheetsReportTask
@@ -6,3 +8,10 @@ from soam.reporting import GSheetsReportTask
 def test_init_fails_if_config_path_is_not_file():
     with pytest.raises(ValueError, match=r".*[Cc]onfig\Wpath.*"):
         GSheetsReportTask("/not_a_real_file.json")
+
+
+def test_init_fails_if_config_path_is_not_json(tmp_path):
+    test_file = tmp_path / f"gsheets_config.json"
+    test_file.write_text("Test text - not json.")
+    with pytest.raises(JSONDecodeError):
+        GSheetsReportTask(test_file)
