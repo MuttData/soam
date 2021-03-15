2f7bf8a4946a3842249ee0f54809493a795c35f6
fabian40 <fabian@muttdata.ai>
2021-03-15 12:47:18 -0300

    add assert to tests

--- tests/workflow/test_store.py
+++ tests/workflow/test_store.py
@@ -15,18 +15,20 @@ TABLE_STORE = "test_store"
 @unittest.skipIf(not os.getenv(TEST_DB_CONNSTR), f"{TEST_DB_CONNSTR} is not set")
 class TestStore(PgTestCase):
     def test_create(self):
+        breakpoint()
         query_create = f"""
-            CREATE TABLE [IF NOT EXISTS] {TABLE_STORE}
+            CREATE TABLE IF NOT EXISTS {TABLE_STORE}
             (date INT,
-            country VARCHAR ( 50 ),
-            metric1 INT,
-                )
+            country VARCHAR (50) ,
+            metric1 INT)
             """
         self.run_query(query_create)
 
         # assert table exist
+        query_assert = f"SELECT to_regclass('{self.db_client.database}.{TABLE_STORE}');"
+        self.assertTrue(self.run_query(query_assert))
 
-    def store_df(self):
+    def store_df(self, mocker):
         df = pd.DataFrame(
             {
                 "date": [1, 2, 3],
@@ -34,10 +36,11 @@ class TestStore(PgTestCase):
                 "metric1": [328, 215, 146],
             }
         )
+        spy = mocker.spy(self.db_client.insert_from_frame)
         s = Store(self.db_client, TABLE_STORE, self.db_client.database)
         s.run(df)
 
-        # assert inserted df
+        spy.assert_called_once_with(df)
 
     @classmethod
     def setUpClass(cls):
