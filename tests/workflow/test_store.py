aede3d96a91512a0cbbaa19487856b18f63fa299
fabian40 <fabian@muttdata.ai>
2021-04-05 12:24:20 -0300

    fix tests

--- tests/workflow/test_store.py
+++ tests/workflow/test_store.py
@@ -4,12 +4,23 @@ import os
 import unittest
 from unittest import main
 
+from muttlib.dbconn import get_client_from_connstr
 import pandas as pd
+from sqlalchemy import Column
+from sqlalchemy.types import Integer, String
 
+from soam.data_models import AbstractIDBase
 from soam.workflow import Store
 from tests.db_test_case import TEST_DB_CONNSTR, PgTestCase
 
 TABLE_STORE = "test_store"
+
+
+class ConcreteTableStore(AbstractIDBase):
+    __tablename__ = TABLE_STORE
+
+    country = Column(String(3))
+    metric1 = Column(Integer())
 
 
 @unittest.skipIf(not os.getenv(TEST_DB_CONNSTR), f"{TEST_DB_CONNSTR} is not set")
@@ -18,7 +29,7 @@ class TestStore(PgTestCase):
         """Test storing data and retriving it."""
         df = pd.DataFrame(
             {
-                "date": [1, 2, 3],
+                "id": [1, 2, 3],
                 "country": ["ARG", "BRZ", "ARG"],
                 "metric1": [328, 215, 146],
             }
@@ -27,29 +38,25 @@ class TestStore(PgTestCase):
         sql_select = f"""
         SELECT * FROM {TABLE_STORE}
         """
-        breakpoint()
-        pd.testing.assert_frame_equal(self.db_client.to_frame(sql_select), df)
+        db_connstr = os.getenv(TEST_DB_CONNSTR)
+        _, db_client = get_client_from_connstr(db_connstr)
+        pd.testing.assert_frame_equal(db_client.to_frame(sql_select), df)
 
     @classmethod
     def setUpClass(cls):
         super().setUp(cls)
-        query_create = f"""
-            CREATE TABLE {TABLE_STORE}
-            (date INT,
-            country VARCHAR (50),
-            metric1 INT)
-            """
-        cls.run_query(query_create)
-        breakpoint()
-        cls.store = Store(cls.db_client, f"{cls.db_client.database}.{TABLE_STORE}")
+
+        db_connstr = os.getenv(TEST_DB_CONNSTR)
+        engine = cls.db_client.get_engine(db_connstr)
+        ConcreteTableStore.__table__.create(engine)
+        cls.store = Store(cls.db_client, f"{TABLE_STORE}")
 
     @classmethod
     def tearDownClass(cls):
         super().tearDown(cls)
-        query_create = f"""
-            DROP TABLE {TABLE_STORE}
-            """
-        cls.run_query(query_create)
+        db_connstr = os.getenv(TEST_DB_CONNSTR)
+        engine = cls.db_client.get_engine(db_connstr)
+        ConcreteTableStore.__table__.drop(engine)
 
     def setUp(self):
         pass
