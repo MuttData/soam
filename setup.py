d014c2c6be2f2f3d61893c456e9680b31dd943d6
Guido Trucco <guido@muttdata.ai>
2021-04-16 14:07:46 +0000

    RFC: Resolve 'Integrate Orbit Wrapper'

--- setup.py
+++ setup.py
@@ -39,7 +39,10 @@ extra_dependencies = {
         'statsmodels<0.12,>=0.11',
     ],
     'slack': ["slackclient",],
-    'prophet': ["pystan==2.19.1.1", "fbprophet==0.6"],
+    'orbit': [
+        'orbit-ml @ git+https://github.com/uber/orbit.git@1a9e38723514255293033541169ce5902aa58d6a'
+    ],
+    'prophet': ["pystan==2.19.1.1", "fbprophet==0.6",],
     'pdf_report': ["jupytext==1.10.2", "papermill==2.3.2", "nbconvert==5.6",],
     'gsheets_report': ["muttlib[gsheets]>=1.0,<2"],
 }
