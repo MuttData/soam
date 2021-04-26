a1117bfa4197e6178d42153603357301c6bb8f27
Wenceslao Villegas <wences@muttdata.ai>
2021-04-26 14:38:03 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- soam/plotting/forecast_plotter.py
+++ soam/plotting/forecast_plotter.py
@@ -120,7 +120,7 @@ class ForecastPlotterTask(Step):
             plot_config=plot_config,
         )
 
-        path = self.path
+        path = path or self.path
         self.path.mkdir(parents=True, exist_ok=True)
         fn = "_".join(
             ["forecast", f"{start_date:%Y%m%d%H}", f"{end_date:%Y%m%d%H}", ".png"]
