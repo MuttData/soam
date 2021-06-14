ffae68e20290f2f6f2f559201250e07475aaa774
Jugo <hviotti@muttdata.ai>
2021-06-14 18:43:43 +0000

    RFC: "Added mlflow tracking Proof of Concept for tasks and flows"

--- soam/core/runner.py
+++ soam/core/runner.py
@@ -11,7 +11,10 @@ TODO: review if SoamFlow is going to be the only class that can run the
 from datetime import datetime
 from typing import TYPE_CHECKING, Optional  # pylint:disable=unused-import
 
+import mlflow
 from prefect import Flow, Task
+
+from soam.cfg import TRACKING_IS_ACTIVE, TRACKING_URI
 
 if TYPE_CHECKING:
     from soam.savers import Saver
@@ -34,12 +37,18 @@ class SoamFlow(Flow):
         kwargs: dict
             extra args.
         """
+
         super().__init__(**kwargs)
         self.saver = saver
         self.start_datetime: datetime
         self.end_datetime: datetime
         if self.saver is not None:
             self.state_handlers.append(self.saver.save_flow_run)
+
+        if TRACKING_IS_ACTIVE:
+            self.active_run = None
+            mlflow.set_tracking_uri(TRACKING_URI)
+            self.state_handlers.append(self.set_tracker_run)
 
     def add_task(self, task: Task) -> Task:
         """
@@ -59,3 +68,28 @@ class SoamFlow(Flow):
             if self.saver.save_task_run not in task.state_handlers:
                 task.state_handlers.append(self.saver.save_task_run)
         return super().add_task(task)
+
+    def set_tracker_run(
+        self, obj, old_state, new_state
+    ):  # pylint: disable=unused-argument
+        """
+        Create a new mlflow run.
+
+        Parameters
+        ----------
+        obj
+            the underlying object to which this state handler is attached
+        old_state
+            the previous state of this object
+        new_state
+            the proposed new state of this object
+        Returns
+        -------
+        newstate
+            the new state of this object.
+        """
+        if new_state.is_running():
+            obj.active_run = mlflow.start_run(run_name="flow_run")
+        if new_state.is_finished():
+            mlflow.end_run()
+        return new_state
