b71ab9842528d169324d4e8cc38937f33ddce67a
Guido Trucco <guido@muttdata.ai>
2021-08-02 16:35:46 +0000

    RFC: Resolve "Add function to create standarized Slack anomaly detection reports"

--- soam/reporting/slack_report.py
+++ soam/reporting/slack_report.py
@@ -10,6 +10,7 @@ from collections.abc import Iterable
 from io import BytesIO
 from pathlib import Path, PosixPath
 from typing import IO, Any, Dict, Optional, Sequence, Union
+import warnings
 
 from jinja2 import Template
 from muttlib.utils import path_or_string
@@ -29,6 +30,11 @@ class SlackReport:
     """
     Generates the report to share via Slack.
     """
+
+    warnings.warn(
+        "This class will be deprecated in v1. Using SlackMessage and send_slack_message methods instead is recommended.",
+        PendingDeprecationWarning,
+    )
 
     def __init__(
         self, channel_id: str, metric_name: str, setting_path: Optional[str],
@@ -79,17 +85,17 @@ class SlackReport:
         if greeting_message == DEFAULT_GREETING_MESSAGE:
             greeting_message.format(metric_name=self.metric_name)
 
-        summary_entries = []
-        summary_entries.append(greeting_message)
-
-        for _, row in prediction.iterrows():
-            date = row[DS_COL].strftime('%Y-%b-%d')
-            value = "{:.2f}".format(row[YHAT_COL])
-            summary_entries.append(f"• *[{date}]* {value}\n")
-
-        summary_entries.append(farewell_message)
-
-        summary_message = "\n".join(summary_entries)  # type: ignore
+        prediction[f"{YHAT_COL}_str"] = prediction[YHAT_COL].apply(
+            lambda f: "{:.2f}".format(f)  # pylint: disable=unnecessary-lambda
+        )
+        summary_message = _df_to_report_string(
+            prediction,
+            DS_COL,
+            f"{YHAT_COL}_str",
+            greeting_message=greeting_message,
+            farewell_message=farewell_message,
+        )
+        prediction = prediction.drop(columns=[f"{YHAT_COL}_str"])
 
         return self.slack_client.files_upload(
             channels=self.channel_id,
@@ -103,6 +109,11 @@ class SlackReportTask(Step, SlackReport):
     """
     Builds up the task of the report designed for Slack.
     """
+
+    warnings.warn(
+        "This class will be deprecated in v1. Using SlackMessage and send_slack_message methods instead is recommended.",
+        PendingDeprecationWarning,
+    )
 
     def __init__(
         self,
@@ -256,3 +267,88 @@ def send_multiple_slack_messages(
             send_slack_messages_in_thread(slack_client, channel, element)
         else:
             send_slack_message(slack_client, channel, element)
+
+
+def _format_number(num):
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    # add more suffixes if you need them
+    return '%.2f%s' % (num, ['', 'k', 'M', 'G', 'T', 'P'][magnitude])
+
+
+def _df_to_report_string(
+    df, date_col, value_col, greeting_message=None, farewell_message=None
+):
+    """Concatenates the rows using the date_col and value_col for formatting purposes."""
+    summary_entries = []
+    if greeting_message:
+        summary_entries.append(greeting_message)
+
+    for _, row in df.iterrows():
+        date = row[date_col].strftime('%Y-%b-%d')
+        value = row[value_col]
+        summary_entries.append(f"• *[{date}]* {value}\n")
+
+    if farewell_message:
+        summary_entries.append(farewell_message)
+
+    summary_message = "\n".join(summary_entries)  # type: ignore
+    return summary_message
+
+
+def send_anomaly_report(
+    slack_client: slack.WebClient,
+    channel_id: str,
+    plot: Union[Path, IO],
+    metric_name: str,
+    anomaly_df: pd.DataFrame,
+    date_col: str,
+):
+    """
+    Parameters
+    ----------
+    channel_id: str
+        Slack channel id where the report will be sent.
+    plot: str, pathlib.Path or buffer
+        Anomaly plot
+    metric_name: str
+        Metric to report.
+    anomaly_df: pd.DataFrame
+        DataFrame with anomalous values. Must have the following columns: ['y','yhat','yhat_lower','yhat_upper']
+    date_col: str
+        Name of the date column
+    """
+    detection_window = len(anomaly_df)
+    stddev = anomaly_df.yhat.std()
+    anomaly_df['stddev'] = abs(anomaly_df.yhat - anomaly_df.y) / stddev
+    anomaly_df['anomaly_upper'] = anomaly_df['y'] > anomaly_df['yhat_upper']
+    anomaly_df['anomaly_lower'] = anomaly_df['y'] < anomaly_df['yhat_lower']
+    aux = anomaly_df.loc[(anomaly_df['anomaly_lower']) | (anomaly_df['anomaly_upper'])]
+    if len(aux) == 0:
+        greeting_message = "Hello everyone! Good news: there were no outliers found for the last {{detection_window}} days for the metric '{{metric_name}}'"
+        msg = SlackMessage(
+            greeting_message,
+            arguments={
+                "detection_window": detection_window,
+                "metric_name": metric_name,
+            },
+            attachment=plot,
+        )
+    else:
+        aux['message'] = aux.apply(
+            lambda row: f"Count: {_format_number(row['y'])}, expected value in range ( {_format_number(row['yhat_lower'])} , {_format_number(row['yhat_upper'])} ) - {row['stddev']:.2f} standard deviations",
+            axis=1,
+        )
+        greeting_message = f"Hello everyone! Here are the outliers found for the last {detection_window} days for the metric '{metric_name}'"
+        summary_message = _df_to_report_string(
+            aux,
+            date_col,
+            'message',
+            greeting_message=greeting_message,
+            farewell_message=None,
+        )
+        msg = SlackMessage(summary_message, attachment=plot)
+
+    send_slack_message(slack_client, channel=channel_id, msg=msg)
