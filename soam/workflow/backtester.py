3d26219a865024025bd5eb0fb545e8e27c65239e
Eugenio <eugenio@muttdata.ai>
2021-09-10 15:39:19 -0300

    DataDummyTransformation on Backtester Corrected

--- soam/workflow/backtester.py
+++ soam/workflow/backtester.py
@@ -134,8 +134,8 @@ class Backtester(Step):
                 # self.state_handlers.append(saver.save_step)
 
         if preprocessor is None:
-            preprocessor = DummyDataFrameTransformer()
-
+            preprocessor = Transformer(DummyDataFrameTransformer())
+            
         self.forecaster = forecaster
         self.preprocessor = preprocessor
         self.forecast_plotter = forecast_plotter
